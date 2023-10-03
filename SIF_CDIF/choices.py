103bb5aaf1b52055cb52c2c6e77dd1eba0318e31
PabloCao1 <89212961+PabloCao1@users.noreply.github.com>
2023-10-02 21:08:54 -0300

    Cambios

--- SIF_CDIF/choices.py
+++ SIF_CDIF/choices.py
@@ -96,3 +96,84 @@ CHOICE_TIPO_DE_FORMULARIO=[
     ('Intervención', 'Intervención'),
     ('Otro', 'Otro'),
 ]
+
+CHOICE_1A5=[
+    (None, ''),
+    ('0', '0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('Más de 5', 'Más de 5'),
+]
+
+CHOICE_EMB_RIESGO=[
+    (None, ''),
+    ('Normal', 'Normal'),
+    ('Adolescente', 'Adolescente'),
+    ('De riesgo (Diabetes, Hipertension, Sífilis, etc.)', 'De riesgo (Diabetes, Hipertension, Sífilis, etc.)'),
+]
+
+CHOICE_EDUCACION=[
+    (None, ''),
+    ('Primario', 'Primario'),
+    ('Secundario', 'Secundario'),
+    ('Terciario', 'Terciario'),
+    ('Universitario', 'Universitario'),
+]
+CHOICE_ESTADO=[
+    (None, ''),
+    ('Completo', 'Completo'),
+    ('Incompleto', 'Incompleto'),
+    ('En curso', 'En curso'),
+    ('No aplica', 'No aplica'),
+]
+CHOICE_PLANES_SOCIALES=[
+    (None, ''),
+    ('AUH (Asignacion Universal por Hijo)', 'AUH (Asignacion Universal por Hijo)'),
+    ('Asignación Universal por Embarazo', 'Asignación Universal por Embarazo'),
+    ('Tarjeta Alimentar', 'Tarjeta Alimentar'),
+    ('Potenciar Trabajo', 'Potenciar Trabajo'),
+    ('Plan Más Vida', 'Plan Más Vida'),
+    ('Plan 1000 Días (ANSES)', 'Plan 1000 Días (ANSES)'),
+    ('Progresar', 'Progresar'),
+    ('Cooperativas', 'Cooperativas'),
+    ('Pensión por discapacidad', 'Pensión por discapacidad'),
+    ('Pensión madre de 7 hijos', 'Pensión madre de 7 hijos'),
+    ('Pensión por viudez', 'Pensión por viudez'),
+]
+
+CHOICE_CONTRATACION=[
+    (None, ''),
+    ('Relación de dependencia', 'Relación de dependencia'),
+    ('Monotributista / Contratado', 'Monotributista / Contratado'),
+    ('Informal con cobro mensual', 'Informal con cobro mensual'),
+    ('Jornal', 'Jornal'),
+    ('Changarín', 'Changarín'),
+    ('Otro', 'Otro'),
+]
+
+CHOICE_SALA_POSTULA=[
+    (None, ''),
+    ('Bebés', 'Bebés'),
+    ('Sala de 2', 'Sala de 2'),
+    ('Sala de 3', 'Sala de 3'),
+]
+CHOICE_TURNO_POSTULA=[
+    (None, ''),
+    ('Mañana', 'Mañana'),
+    ('Tarde', 'Tarde'),
+]
+CHOICE_TIPO_IVI=[
+    (None, ''),
+    ('Madre o Cuidador principal', 'Madre o Cuidador principal'),
+    ('Bebé, niño o niña', 'Bebé, niño o niña'),
+    ('Familia', 'Familia'),
+    ('Ajustes', 'Ajustes'),
+]
+CHOICE_NOSI=[
+    (None, ''),
+    ('SI', 'SI'),
+    ('NO', 'NO'),
+]
