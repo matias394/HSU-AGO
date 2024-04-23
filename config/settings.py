0799078feec7253f5a2031ccf5b0dd5cd4d9835a
Pablo <pablocao@gmail.com>
2024-04-23 01:54:01 -0300

    cambios

--- config/settings.py
+++ config/settings.py
@@ -130,27 +130,27 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hsu-dev',
-        'USER': 'hsudev',
-        'PASSWORD': 'hsudevtest',
-        'HOST': '172.20.30.189',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-        'CONN_MAX_AGE': 300,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'hsu-dev',
+#        'USER': 'hsudev',
+#        'PASSWORD': 'hsudevtest',
+#        'HOST': '172.20.30.189',
+#        'PORT': '3306',
+#        'OPTIONS': {
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#            'charset': 'utf8mb4',
+#        },
+#        'CONN_MAX_AGE': 300,
+#    }
+#}
 
 
 # Password validation
