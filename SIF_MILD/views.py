c98746a1c21049758666d83fe3f2f6f40d58a472
nehuen871 <nehuennavarro@gmail.com>
2024-09-25 19:41:28 -0300

    cambios

--- SIF_MILD/views.py
+++ SIF_MILD/views.py
@@ -1434,15 +1434,14 @@ class MILDPreAdmisiones3DetailView(PermisosMixin, DetailView):
             
             legajoMadre = LegajoGrupoFamiliar.objects.filter(fk_legajo_1_id=preadmi.fk_legajo_id,vinculo="Madre").first()
             alertaId = Alertas.objects.filter(nombre="Hijo en programa mil dias").first()
-
-            validacionMadre = LegajoAlertas.objects.filter(fk_legajo_id=legajoMadre.fk_legajo_2.id, fk_alerta_id=alertaId.id).first()
-            
-            if validacionMadre is None:
-                legajosAlertasMadre = LegajoAlertas()
-                legajosAlertasMadre.fk_legajo_id = legajoMadre.fk_legajo_2.id
-                legajosAlertasMadre.creada_por_id = self.request.user.id
-                legajosAlertasMadre.fk_alerta_id = alertaId.id
-                legajosAlertasMadre.save()
+            if legajoMadre is None:
+                validacionMadre = LegajoAlertas.objects.filter(fk_legajo_id=legajoMadre.fk_legajo_2.id, fk_alerta_id=alertaId.id).first()
+                if validacionMadre is None:
+                    legajosAlertasMadre = LegajoAlertas()
+                    legajosAlertasMadre.fk_legajo_id = legajoMadre.fk_legajo_2.id
+                    legajosAlertasMadre.creada_por_id = self.request.user.id
+                    legajosAlertasMadre.fk_alerta_id = alertaId.id
+                    legajosAlertasMadre.save()
             
             preadmi.admitido = "SI"
             preadmi.estado = "Admitido"
