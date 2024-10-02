1b97c3399f160e3da4982df556103a7185652567
albakore <albakoreofficial@gmail.com>
2024-10-01 22:19:42 -0300

    [Feature] HSU-1500 Intervencion fecha requerido

--- SIF_PDV/forms.py
+++ SIF_PDV/forms.py
@@ -184,7 +184,7 @@ class PDV_IntervencionesForm (forms.ModelForm):
         widgets = {
             'detalle': forms.Textarea(attrs={'class': 'form-control','rows': 3,}),
             'responsable' : forms.SelectMultiple(attrs={'class': 'select2 w-100', 'multiple': True}),
-            'fecha': forms.DateInput(attrs={'type': 'date'}, format="%Y-%m-%d"),
+            'fecha': forms.DateInput(attrs={'type': 'date', 'required':True}, format="%Y-%m-%d"),
         }
         labels = {
             'criterio_modificable': 'Criterio modificable trabajado',
